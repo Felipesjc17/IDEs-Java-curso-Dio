e4c6c4ff22c93379eacb49f621de44db49b7ade2
Felipesjc17 <felipe_sjc17@hotmail.com>
2021-07-18 20:27:42 -0300

    first commit

---
+++ src/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,33 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+
+
+		Gato gato = new Gato();
+
+		
+		System.out.println(gato);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+/*
+
+ctrl espaço completa
+ctrl 3 aparece opções para dd
+ctrl N novo
+ctrl D apaga linha
+
+*/
